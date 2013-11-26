540fd7a979b21ec6ad12d2152f5891e16afa495a
Salah Waisi <salahwaisi@gmail.com>
2013-11-26 15:48:42 +0100

    Added all files

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "public/styles/css"
+sass_dir = "assets"
+images_dir = "public/styles/img"
+javascripts_dir = "public/js/"
+output_style = :compressed
+line_comments = false
---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<link rel="apple-touch-icon" href="img/icons/touch-icon-iphone.png">
+
+	<link rel="stylesheet" type="text/css" href="styles/css/main.css">
+	<link rel="stylesheet" type="text/css" href="styles/css/screen.css">
+</head>
+<body>
+
+<!--[if lt IE 10]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please
+    <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+<script src="js/main.js"></script>
+
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+</body>
+</html>
